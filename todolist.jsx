165972936e78cf2734089dacb06febde8e4023e7
Vinicius <vinicius.legramanti@gmail.com>
2023-11-19 23:19:04 -0700

    update todolist.jsx

---
+++ todolist.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { ScrollView, Pressable, View, Text, StyleSheet } from "react-native";
+
+function ToDoList({ tasks }) {
+    return (
+        <ScrollView>
+            {tasks.map((task, index) => (
+                <Pressable key = {index}>
+                <View style = {[styles.task, styles.completed]}>
+                    <Text style = {styles.taskText}>{task} </Text>
+                </View>
+                </Pressable>
+            ))}
+      </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    task: {
+      padding: 15,
+      borderBottomWidth: 1,
+      borderColor: '#ccc',
+    },
+    completed: {
+      backgroundColor: '#e0e0e0',
+    },
+    taskText: {
+      fontSize: 16,
+    },
+});
+
+export default ToDoList;
